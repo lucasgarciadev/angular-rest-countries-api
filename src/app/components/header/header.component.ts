9e2697ace8c19e6908f5499b406c1a05924329c3
Lucas Garcia <lukas.garcia52@gmail.com>
2023-07-21 00:28:35 -0300

    :construction: Incluindo mudanca de tema

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -10,4 +10,15 @@ import { faMoon } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   faMoon = faMoon;
+  faSun = faSun;
+  darkMode: boolean = false;
+
+  toggleTheme() {
+
+    this.darkMode = !this.darkMode;
+
+    document.documentElement.setAttribute('data-theme', this.darkMode ? 'dark' : 'light')
+
+    // document.body.classList.toggle('dark-theme');
+  }
 }
