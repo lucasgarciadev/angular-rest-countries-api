92d5b37ba3ee53c090de785a54924f2078552593
Lucas Garcia <lukas.garcia52@gmail.com>
2023-08-05 01:02:34 -0300

    :bug: atualizando importacao do componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './components/list-card/card/card.component';
 import { ListCardComponent } from './components/list-card/list-card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
